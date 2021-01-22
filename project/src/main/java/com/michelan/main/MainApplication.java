3b11e0286659ede969961f59e204ad223904191a
gust9988 <gust9988@hanmail.net>
2021-01-22 18:07:12 +0900

    배포 성공

--- project/src/main/java/com/michelan/main/MainApplication.java
+++ project/src/main/java/com/michelan/main/MainApplication.java
@@ -1,9 +1,12 @@
 package com.michelan.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+
 public class MainApplication {
 
 	public static void main(String[] args) {
