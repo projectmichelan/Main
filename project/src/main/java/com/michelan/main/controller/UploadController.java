c183e5a815af70de275a38d13024d6f41e0b6b1a
kimwram <kimwheeram@naver.com>
2021-01-26 15:31:54 +0900

    load

--- project/src/main/java/com/michelan/main/controller/UploadController.java
+++ project/src/main/java/com/michelan/main/controller/UploadController.java
@@ -1,8 +1,11 @@
 package com.michelan.main.controller;
 
 
+import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
+import org.apache.tomcat.jni.File;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -11,7 +14,19 @@ public class UploadController {
     @GetMapping("/upload")
     public String upload() {
 
+
         return "upload";
     }
+    @PostMapping("/upload") public String upload(@RequestParam("file") MultipartFile multipartFile) {
+        log.info("upload");
+        File targetFile = new File(path.resolve(multipartFile.getOriginalFilename()).toString());
+        try {
+            InputStream fileStream = multipartFile.getInputStream(); FileUtils.copyInputStreamToFile(fileStream, targetFile);
+        } catch (IOException e) {
+            FileUtils.deleteQuietly(targetFile);
+            log.error("Failed to upload ", e);
+        }
+        return "redirect:/form"; }
+
 
 }
